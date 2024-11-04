7bf81e6b67b4ef7fd3d0cf014add4983f1386a67
Kitikorn Khanthasen <evillgodmrsupersonic@gmail.com>
2024-11-04 15:56:02 +0700

    write method=PUT

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 
 URL = 'https://jsonplaceholder.typicode.com/users'
 API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
+API_ENDPOINT_PUT = 'https://jsonplaceholder.typicode.com/posts/1'
 
 # target = "thailand"
 
@@ -15,9 +16,21 @@ API_ENDPOINT = 'https://jsonplaceholder.typicode.com/posts'
 
 # print(filtered_users)
 
-data = {'name': 'Satee'}
+# data = {'name': 'Satee'}
+# headers = {'Content-type': 'application/json; charset=UTF-8'}
+
+# req = requests.post(url=API_ENDPOINT, json=data, headers=headers)
+
+# response 201 = success
+# check more HTTP code status in this link : https://restfulapi.net/http-status-codes/
+# print(req.json())
+
+payload = { 'id': 1, 
+           'title': 'foo',
+           'body': 'bar',
+           'userId': 1
+          }
 headers = {'Content-type': 'application/json; charset=UTF-8'}
 
-req = requests.post(url=API_ENDPOINT, json=data, headers=headers)
-
-print(req.json())
+res = requests.put(url=API_ENDPOINT_PUT, json=payload, headers=headers)
+print(res.json())
